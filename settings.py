2e5783f6197e4ae70e802d0d61646abab12be4d9
stilix1 <49125939+stilix1@users.noreply.github.com>
2024-05-28 20:41:06 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 # TG Token
-Token = "6811284634:AAGxKtj2ErPAq7akQoWu6S6_YkmfB8Y3818"
-bot_name = "strip_master_bot"
+Token = ""
+bot_name = ""
 menu_photo_path = './tmp/files/logo/logo.png'
 
 # RUkassa
-RUKASSA_TOKEN = '01a57fb601278a19c97a0dc04a6ab9c4'
-RUKASSA_SHOP_ID = '2327'
+RUKASSA_TOKEN = ''
+RUKASSA_SHOP_ID = ''
 
 # Учетные данные для авторизации
-username = 'fishing'
-password = 'e04efaae38c164c30b39'
+username = ''
+password = 'e'
