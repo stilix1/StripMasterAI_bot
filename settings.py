15adb1ad0935b437b13d2e509e22b03178a25864
stilix1 <49125939+stilix1@users.noreply.github.com>
2024-06-15 19:26:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,19 @@
 # TG Token
-Token = ""
-bot_name = ""
-menu_photo_path = './tmp/files/logo/logo.png'
+Token = "7195070913:AAF1ubcpgalDq1jtK5dnDqTfD9r_Tjo1t2c"
+bot_name = "strip_master_bot"
+
 
 # RUkassa
-RUKASSA_TOKEN = ''
-RUKASSA_SHOP_ID = ''
+RUKASSA_TOKEN = '01a57fb601278a19c97a0dc04a6ab9c4'
+RUKASSA_SHOP_ID = '2327'
 
-# Учетные данные для авторизации
-username = ''
-password = 'e'
+
+# AAIO
+AAIO_TOKEN = 'N2NhOWI2ZjAtNzYzOS00MGFkLWE2NDUtZjIzMTVjZjA4ZjFjOkB1eURFdkJIVkRYNWVVdmJWa2l1aHhzbSU4QXBJaF5X'
+AAIO_SHOP_ID = '8b457290-14d9-4673-ba72-10945d8e1245'
+AAIO_secret1 = 'c5a2c0a503f471c049be11eef2920878'
+AAIO_secret2 = 'c1f29962a47831a033e1723dee302939'
+
+# Учетные данные для авторизации AI
+username = 'fishing'
+password = 'e04efaae38c164c30b39'
