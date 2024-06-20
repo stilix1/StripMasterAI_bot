eab77abe10922b21c39b206c75f3a115bd307bc2
stilix1 <49125939+stilix1@users.noreply.github.com>
2024-06-20 15:33:55 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from datetime import datetime, timedelta
 
 import asyncpg
 
@@ -14,7 +15,8 @@ CREATE SCHEMA IF NOT EXISTS core;
 CREATE SCHEMA IF NOT EXISTS data;
 
 CREATE TABLE IF NOT EXISTS core.users (
-    id BIGINT PRIMARY KEY,  -- Указываем, что тип данных BIGINT
+    serial_id SERIAL PRIMARY KEY,
+    id BIGINT UNIQUE NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     language_code VARCHAR(10),
@@ -24,12 +26,13 @@ CREATE TABLE IF NOT EXISTS core.users (
     invited_by BIGINT  -- BIGINT, совместимо с `id`
 );
 
+
 CREATE TABLE IF NOT EXISTS core.credits (
     user_id BIGINT PRIMARY KEY REFERENCES core.users(id),  -- Теперь здесь BIGINT
     credits_paid INTEGER DEFAULT 0,
     credits_free INTEGER DEFAULT 0,
     credits_ref INTEGER DEFAULT 0
-);
+);  
 
 CREATE TABLE IF NOT EXISTS core.invites (
     user_id BIGINT REFERENCES core.users(id),  -- BIGINT, совместимо с `id`
@@ -52,6 +55,14 @@ CREATE TABLE IF NOT EXISTS data.processings (
     status VARCHAR(50),
     paid BOOLEAN
 );
+
+CREATE TABLE IF NOT EXISTS data.referral_transactions (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES core.users(id),
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    amount INTEGER,
+    status VARCHAR(50)
+);
 """
 
 
@@ -60,6 +71,7 @@ async def create_pool():
                                      database=DB_DATABASE, host=DB_HOST)
 
 
+# Запросы к базе данных
 async def add_user(pool, user_id, username, language_code, is_premium, ref_link=None, invited_by=None):
     async with pool.acquire() as conn:
         try:
@@ -88,19 +100,169 @@ async def add_user(pool, user_id, username, language_code, is_premium, ref_link=
             raise
 
 
+async def get_total_referral_topups_by_id(pool, user_id):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT COALESCE(SUM(rt.amount), 0) FROM data.referral_transactions rt
+            JOIN core.users u ON rt.user_id = u.id
+            WHERE u.invited_by = $1;
+        '''
+        total_referral_topups = await conn.fetchval(query, user_id)
+        return total_referral_topups
+
+
+async def get_all_registered_users(pool):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT id FROM core.users;
+        '''
+        rows = await conn.fetch(query)
+        return [row['id'] for row in rows]
+
+
+async def get_new_users_from_referral_by_id(pool, user_id):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT COUNT(*) FROM core.users
+            WHERE invited_by = $1;
+        '''
+        new_users_count = await conn.fetchval(query, user_id)
+        return new_users_count
+
+
+async def get_users_with_balance_from_referral_by_id(pool, user_id):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT COUNT(DISTINCT u.id) FROM core.users u
+            JOIN data.referral_transactions rt ON u.id = rt.user_id
+            WHERE u.invited_by = $1;
+        '''
+        users_with_balance = await conn.fetchval(query, user_id)
+        return users_with_balance
+
+
+async def get_referrer_id(pool, user_id):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT invited_by FROM core.users
+            WHERE id = $1;
+        '''
+        referrer_id = await conn.fetchval(query, user_id)
+
+    return referrer_id
+
+
+async def record_referral_transaction(pool, user_id, amount, status):
+    async with pool.acquire() as conn:
+        try:
+            await conn.execute('''
+                INSERT INTO data.referral_transactions (user_id, amount, status)
+                VALUES ($1, $2, $3);
+            ''', int(user_id), amount, status)
+            logging.info(f"Referral transaction recorded: user {user_id} credited with {amount}")
+        except Exception as e:
+            logging.error(f"Error recording referral transaction in DB: {e}")
+            raise
+
+
 async def add_referral_credits(pool, inviter_id):
     async with pool.acquire() as conn:
         try:
             # Увеличение credits_ref для пригласившего пользователя
             await conn.execute('''
                 UPDATE core.credits
-                SET credits_ref = credits_ref + 20
+                SET credits_free = credits_ref + 20
                 WHERE user_id = $1;
             ''', int(inviter_id))
             logging.info(f"Referral credits added: {inviter_id} gets 20 credits")
         except Exception as e:
             logging.error(f"Error adding referral credits in DB: {e}")
             raise
+
+
+async def get_new_users_from_referral(pool):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT COUNT(*) FROM core.users
+            WHERE invited_by IS NOT NULL;
+        '''
+        new_users_count = await conn.fetchval(query)
+        return new_users_count
+
+async def get_users_with_balance_from_referral(pool):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT COUNT(DISTINCT u.id) FROM core.users u
+            JOIN data.referral_transactions rt ON u.id = rt.user_id;
+        '''
+        users_with_balance = await conn.fetchval(query)
+        return users_with_balance
+
+
+async def get_total_referral_topups(pool):
+    async with pool.acquire() as conn:
+        query = '''
+            SELECT COALESCE(SUM(rt.amount), 0) FROM data.referral_transactions rt;
+        '''
+        total_referral_topups = await conn.fetchval(query)
+        return total_referral_topups
+
+
+async def get_total_users(pool):
+    async with pool.acquire() as conn:
+        result = await conn.fetchval('SELECT COUNT(*) FROM core.users')
+        return result
+
+
+async def get_users_last_week(pool):
+    async with pool.acquire() as conn:
+        one_week_ago = datetime.now() - timedelta(weeks=1)
+        result = await conn.fetchval('SELECT COUNT(*) FROM core.users WHERE created_at >= $1', one_week_ago)
+        return result
+
+
+async def get_users_last_month(pool):
+    async with pool.acquire() as conn:
+        one_month_ago = datetime.now() - timedelta(days=30)
+        result = await conn.fetchval('SELECT COUNT(*) FROM core.users WHERE created_at >= $1', one_month_ago)
+        return result
+
+
+async def get_all_users(pool):
+    async with pool.acquire() as conn:
+        query = '''
+        SELECT u.serial_id, u.id, u.username, u.created_at, u.selected_language, u.ref_link, u.invited_by,
+               c.credits_free, c.credits_paid, c.credits_ref
+        FROM core.users u
+        LEFT JOIN core.credits c ON u.id = c.user_id;
+        '''
+        result = await conn.fetch(query)
+        return result
+
+
+async def get_referral_stats(pool, user_id):
+    async with pool.acquire() as conn:
+        # Преобразуем user_id в int, если он не был int
+        user_id = int(user_id)
+
+        # Запрос для получения количества приглашенных пользователей
+        query_referrals = '''
+            SELECT COUNT(*) FROM core.invites
+            WHERE user_id = $1;
+        '''
+        referrals = await conn.fetchval(query_referrals, user_id)
+
+        # Запрос для получения общей суммы реферальных кредитов
+        query_total_credit = '''
+            SELECT COALESCE(SUM(rt.amount), 0) FROM data.referral_transactions rt
+            JOIN core.users u ON rt.user_id = u.id
+            WHERE u.id IN (
+                SELECT invited FROM core.invites WHERE user_id = $1
+            );
+        '''
+        total_referral_credit = await conn.fetchval(query_total_credit, user_id)
+
+    return referrals, total_referral_credit
 
 
 async def set_language(pool, user_id, language):
@@ -219,7 +381,7 @@ async def get_user_profile_data(pool, user_id):
         # Извлекаем данные о пользователе из таблицы core.users и core.credits
         query = """
         SELECT u.username, u.created_at, u.ref_link, 
-                c.credits_free,
+                c.credits_ref,
                (c.credits_paid + c.credits_free + c.credits_ref) as balance
         FROM core.users u
         LEFT JOIN core.credits c ON u.id = c.user_id
@@ -232,7 +394,7 @@ async def get_user_profile_data(pool, user_id):
                 'created_at': result['created_at'].strftime('%Y-%m-%d %H:%M:%S'),
                 'ref_link': result['ref_link'],
                 'balance': result['balance'],
-                'ref_balance': result['credits_free']
+                'ref_balance': result['credits_ref']
             }
         else:
             return None  # В случае отсутствия пользователя в базе
